9513500ea6e04c83751dfca37c06914bdab35bac
EvianMckeown <evian.mck@gmail.com>
2024-05-11 22:48:16 +0200

    SJF scheduler per drink implemented

--- OS2_skeletonCode/OS2_skeletonCode/src/barScheduling/Barman.java
+++ OS2_skeletonCode/OS2_skeletonCode/src/barScheduling/Barman.java
@@ -15,50 +15,50 @@ Barman Thread class.
 */
 
 public class Barman extends Thread {
-	
+
 	private CountDownLatch startSignal;
 	private BlockingQueue<DrinkOrder> orderQueue;
-	//private PriorityBlockingQueue<DrinkOrder> orderArray;
+	// private PriorityBlockingQueue<DrinkOrder> orderArray;
 	private int schedAlg;
-	
+
 	Barman(CountDownLatch startSignal, int schedAlg) {
 		this.schedAlg = schedAlg;
 		if (schedAlg == 0) {
 			this.orderQueue = new LinkedBlockingQueue<>();
 		}
 		// FIX below
-		if (schedAlg == 1) {
+		else if (schedAlg == 1) {
 			// SJF
-			/* We need arrival time and burst time...
-			-	burst time is how many drinks a patron has ordered (?)
-			-	arrival time is if in queue or not at certain point
-			*/
-			this.orderQueue = new PriorityBlockingQueue<>(Integer.MAX_VALUE,new Comparator<DrinkOrder>() {
+			/*
+			 * We need arrival time and burst time...
+			 * - burst time is how many drinks a patron has ordered (?)
+			 * - arrival time is if in queue or not at certain point
+			 */
+			this.orderQueue = new PriorityBlockingQueue<>(11, new Comparator<DrinkOrder>() {
 				@Override
 				public int compare(DrinkOrder o1, DrinkOrder o2) {
 					return Integer.compare(o1.getExecutionTime(), o2.getExecutionTime());
 				}
 			});
-			
-		} else
-		// default to FIFO
-		this.schedAlg = 0;
-		this.orderQueue = new LinkedBlockingQueue<>(); // this just does the same thing
-		
+		} else {
+			// default to FIFO
+			this.schedAlg = 0;
+			this.orderQueue = new LinkedBlockingQueue<>(); // this just does the same thing
+		}
 		this.startSignal = startSignal;
 	}
-	
+
 	public void placeDrinkOrder(DrinkOrder order) throws InterruptedException {
 		orderQueue.put(order);
 	}
-	
+
 	public void run() {
 		try {
 			DrinkOrder nextOrder;
-			
+
 			startSignal.countDown(); // barman ready
 			startSignal.await(); // check latch - don't start until told to do so
-			
+
 			while (true) {
 				if (schedAlg == 0) {
 					nextOrder = orderQueue.take();
@@ -67,7 +67,7 @@ public class Barman extends Thread {
 					System.out.println("---Barman has made order for patron " + nextOrder.toString());
 					nextOrder.orderDone();
 				} else {
-					// schedAlg = 1 
+					// schedAlg = 1
 					// SJF Scheduling
 					nextOrder = orderQueue.take();
 					System.out.println("---Barman preparing order for patron " + nextOrder.toString());
@@ -76,7 +76,7 @@ public class Barman extends Thread {
 					nextOrder.orderDone();
 				}
 			}
-			
+
 		} catch (InterruptedException e1) {
 			System.out.println("---Barman is packing up ");
 		}
